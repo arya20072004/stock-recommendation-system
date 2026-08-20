da7dcb30bff58298b772a5f1c91d1cf7a67a2091
Arya Borhade <aryaborhade20@gmail.com>
2026-08-20 15:38:22 +0530

    chore: remove obsolete repository artifacts

--- src/config/config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-ALPHA_VANTAGE_API_KEY = os.getenv('ALPHA_VANTAGE_API_KEY')
-NEWS_API_KEY = os.getenv('NEWS_API_KEY')
-MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
